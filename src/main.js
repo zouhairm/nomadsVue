2c0fddaa5c7573075991361759758647ee83f544
zouhair <zouhair.mahboubi@gmail.com>
2019-08-04 23:53:38 -0400

    adding google analytics

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 
 
+import VueAnalytics from 'vue-analytics'
+Vue.use(VueAnalytics, {
+  id: 'UA-144701829-1',
+  debug: { sendHitTask: process.env.NODE_ENV === 'production'}
+})
+
+
 //1st, configure font-awesome
 import { library } from '@fortawesome/fontawesome-svg-core' //get library and add icons of interest
 import { fas } from '@fortawesome/free-solid-svg-icons'; //we want the free icons
@@ -10,8 +17,8 @@ library.add(fas); //add the free icons
 //2nd, get the vue <-> font awesome component and register it with the app
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 Vue.component('fa-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
